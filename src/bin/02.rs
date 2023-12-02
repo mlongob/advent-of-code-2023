6266472d265a5d86b80b247aeb9521fa16baf0de
Mario Longobardi <mlongobardi2@bloomberg.net>
2023-12-02 16:20:24 -0500

    Fix test

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -52,7 +52,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(Some(8), None);
+        assert_eq!(result, Some(8));
     }
 
     #[test]
